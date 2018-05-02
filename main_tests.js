4c490643f5e30e223a17f881c2390ab6b2d770b2
Ditmir Hasani <ditmir.hasani@bbc.co.uk>
2018-05-02 18:41:53 +0100

    add main_tests outside folder

---
+++ main_tests.js
@@ -0,0 +1,14 @@
+var conf = require('./nightwatch.conf.js');
+
+module.exports = {
+    'check page load': function (browser) {
+        browser
+            .url('http://www.cinego.uk/')   // visit the url
+            .waitForElementVisible('body');
+        }
+    }
+//
+// var i;
+// for (i = 1000;i > 0 ; i --) {
+//     browser.url('https://cinego.uk/news-updates/10-must-see-movies-in-2018/')
+//     console.log(i)
