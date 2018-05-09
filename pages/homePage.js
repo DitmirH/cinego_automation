735952fcf02f16030d6475d9733ea006b9344ff0
Ditmir Hasani <ditmir.hasani@bbc.co.uk>
2018-05-09 10:02:18 +0100

    add 2nd tests for navigating to my list

---
+++ pages/homePage.js
@@ -0,0 +1,15 @@
+//
+// module.exports = {
+//     elements: {
+//         primus: {
+//             selector: '#nav-link-prime'
+//         }
+//
+//     }
+// };
+
+module.exports = {
+    elements: {
+        primus: '#nav-link-prime'
+    }
+};
